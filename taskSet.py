35247c773222b099cab96f26ba6fd8d2c54ca916
El-akama <akmatjanovel@gmail.com>
2020-11-02 15:17:45 +0600

    initial commit

---
+++ taskSet.py
@@ -0,0 +1,42 @@
+# task1
+# a = {1, 6, 4, 0, 34, 17, 100, -45}
+# b = {22, 0, 3, 100, 4}
+# p = a & b
+# d = a - b
+# u = a | b
+# print(p)
+# print(d)
+# print(u)
+
+# task2
+# Aibek = {'dodo', 'imperia', 'freshbox'}
+# Aidai = {'ochakkebab', 'freshbox'}
+# Azat = {'freshbox', 'kfc'}
+# Gulzana = Aibek | Aidai | Azat
+# mesto = Aibek.intersection(Aidai, Azat, Gulzana)
+# print(mesto)
+
+# task3
+# John = {5, 7, 11, 10, 28}
+# Rachel = {29, 2, 6, 12, 3}
+# Katrine = {1, 5, 14, 8, 22}
+# Sam = {19, 20, 3, 11, 10}
+# if John.intersection(Rachel):
+#     print('Rachel')
+# if John.intersection(Katrine):
+#     print('Katrine')
+# if John.intersection(Sam):
+#     print('Sam')
+# print('these people are right')
+
+# task4
+# ingredient = {'сыр чеддар', 'грибы', 'соус', 'шпинат'}
+# ingredient.add('помидоры')
+# if 'колбаса' in ingredient:
+#     ingredient.remove()
+# ingredient.discard('шпинат')
+# ingredient.add('базилик')
+# ingredient.discard('сыр чеддар')
+# ingredient.add('сыр моцарелла')
+# print(ingredient)
+
